10ff0808bcb341b805d7fed2edd33e4fd7d0a04b
Mohd Danish <63279839+mohddanish-cse@users.noreply.github.com>
2021-06-05 15:25:35 +0530

    feat: completed building the app
    feat: completed building the app, need to design it

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput =document.querySelector("#txt-input");
 var outputDiv = document.querySelector('#output');
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+function getTransationURL(text) {
+    return serverURL+"?=text"+text
+}
 
 function clickHandler() {
-    outputDiv.innerHTML="hssh "+txtInput.value;
+    var inputText=txtInput.value;
+
+    fetch(getTransationURL(inputText))
+    .then(response => response.json())
+    .then(json =>outputDiv.innerText=json.contents.translation)
 }
 
 btnTranslate.addEventListener("click",clickHandler);
 
+// json =>outputDiv.innerText=json.contents.translation
