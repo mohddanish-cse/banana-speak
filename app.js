877a0e6d3b97c2fa5f7f4d97b18b8f423a3ae237
Mohd Danish <63279839+mohddanish-cse@users.noreply.github.com>
2021-06-11 13:21:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput =document.querySelector("#txt-input");
 var outputDiv = document.querySelector('#output');
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-// var serverURL = "https://api.funtranslations.com/translate/minion.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTransationURL(text) {
     console.log(serverURL+"?"+"text="+text);
