729e73bef97a242d3ed1dce14f800849a392f72f
Mohd Danish <63279839+mohddanish-cse@users.noreply.github.com>
2021-08-24 19:02:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput =document.querySelector("#txt-input");
-var outputDiv = document.querySelector('#output');
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+// referencing the Input, output and button from HTMl
 
-function getTransationURL(text) {
-    console.log(serverURL+"?"+"text="+text);
-    return serverURL+"?"+"text="+text
+var btnTranslate = document.querySelector("#btn-translate")
+var inputText= document.querySelector("#txt-input")
+var outputDiv= document.querySelector("#output")
+
+
+// the API URL from funtranslations.com
+
+var url= "https://api.funtranslations.com/translate/minion.json"
+
+// constructing the url for fetch
+function getTranslationUrl(input) {
+    return url+ "?"+ "text="+ input
 }
 
-function clickHandler() {
-    var inputText=txtInput.value;
+// a function to handle errors
 
-    fetch(getTransationURL(inputText))
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("server not responding, try again later!")
+}
+
+// main clickHandler function that takes the input, sends the request to sever and gets the translated texxt back
+
+function clickHandler(){
+    var inputTxt= inputText.value;
+    
+    fetch(getTranslationUrl(inputTxt)) 
     .then(response => response.json())
-    .then(json =>outputDiv.innerText=json.contents.translation)
+    .then(json => {
+        var translatedText= json.contents.translated;
+        outputDiv.innerText= translatedText;
+    } )
+    .catch(errorHandler)
 }
 
-btnTranslate.addEventListener("click",clickHandler);
-
+// when the user clicks on the Translate button, the clickHandler function gets called
+ btnTranslate.addEventListener("click",  clickHandler)
